063de31563dd61255f56c911d016e83c7cfef571
pgossman <pgossman@umich.edu>
2019-12-04 16:09:28 -0500

    give credit

--- .setup_dotfiles_repo.sh
+++ .setup_dotfiles_repo.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
+# Thanks to this article for helping me setup git for dotfiles so elegantly
+# https://medium.com/toutsbrasil/how-to-manage-your-dotfiles-with-git-f7aeed8adf8b
+
 if [[ -d .dotfiles ]]; then
     echo "~/.dotfiles already exists, not setting up"
     exit 1
