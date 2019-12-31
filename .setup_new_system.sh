32422a0054904a9d10b607e81f2c59aa4c835ed1
Paul Gossman <me@paulgossman.com>
2019-12-30 22:03:13 -0500

    New script for initializing packages on a new system

---
+++ .setup_new_system.sh
@@ -0,0 +1,23 @@
+sudo apt-get install \
+	curl \
+	openssh-server \
+	zsh \
+	vim \
+	emacs \
+	tmux \
+	git \
+	python3 \
+	python-pip \
+	python3-pip \
+	i3 \
+	feh \
+	compton
+
+pip3 install black
+
+git config --global user.name "Paul Gossman"
+git config --global user.email "me@paulgossman.com"
+
+echo "Remaining things to do:"
+echo "- Setup ssh keys"
+echo "- Clone dotfiles and run setup script"
