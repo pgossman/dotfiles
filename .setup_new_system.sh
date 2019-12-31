148fd28c25cd22f792b47213ba67293243509c11
Paul Gossman <me@paulgossman.com>
2019-12-30 22:08:21 -0500

    install oh-my-zsh

--- .setup_new_system.sh
+++ .setup_new_system.sh
@@ -18,6 +18,8 @@ pip3 install black
 git config --global user.name "Paul Gossman"
 git config --global user.email "me@paulgossman.com"
 
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 echo "Remaining things to do:"
 echo "- Setup ssh keys"
 echo "- Clone dotfiles and run setup script"
