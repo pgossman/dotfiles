67d53515b69b2f561d03109c9a5098a1932ca93d
Paul Gossman <me@paulgossman.com>
2020-04-16 05:57:10 -0400

    i3lock: add blue color to background color set

--- .scripts/i3lock_color.sh
+++ .scripts/i3lock_color.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#       orange   lavender  purple  sky blue
-colors=("e9a92b" "d014ff" "ff00ff" "00ffc1")
+#       orange   lavender  purple   algae    blue   
+colors=("e9a92b" "d014ff" "ff00ff" "00ffc1" "1877c9")
 
 rand=$$$(date +%s)
 echo ${colors[$rand % ${#colors[@]} ]}
